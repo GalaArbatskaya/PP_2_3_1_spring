253223763c54085c3311a43d1fa4848cb6c5b287
Gala Arbatskaya <galinaarbatskaya0@gmail.com>
2023-05-27 23:55:53 +0300

    torment 1

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,18 @@
+package web.dao;
+
+import web.models.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    User getUserById(long id);
+
+    void save(User user);
+
+    void update(User user);
+}
